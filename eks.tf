b4eea0adca6486ea04d37942f32195a650e13385
Ruchita Dhokare <dhokareruchita20@gmail.com>
2026-06-11 13:10:37 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -8,11 +8,11 @@ resource "aws_iam_role" "cluster" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
+      Effect = "Allow"
       Action = [
         "sts:AssumeRole",
         "sts:TagSession"
       ]
-      Effect = "Allow"
       Principal = {
         Service = "eks.amazonaws.com"
       }
@@ -21,33 +21,40 @@ resource "aws_iam_role" "cluster" {
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "default" {
+data "aws_subnets" "eks" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = [
+      "us-east-1a",
+      "us-east-1b"
+    ]
   }
 }
 
 resource "aws_eks_cluster" "cluster" {
   name     = "cluster"
   role_arn = aws_iam_role.cluster.arn
+  version  = "1.33"
 
   access_config {
     authentication_mode = "API"
   }
 
-  version = "1.33"
-
   vpc_config {
-    subnet_ids = data.aws_subnets.default.ids
+    subnet_ids = data.aws_subnets.eks.ids
   }
 
   depends_on = [
